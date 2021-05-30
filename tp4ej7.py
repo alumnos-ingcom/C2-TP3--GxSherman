e6859c9afb19c274a669c689b5cc21ff33197cb5
GxSherman <69094903+GxSherman@users.noreply.github.com>
2021-05-30 20:12:25 -0300

    commit 1 ej 7 y 8

---
+++ tp4ej7.py
@@ -0,0 +1,29 @@
+################
+# Germán Bernhard @GermanBernhard
+# TP4 - Ejercicio 7 - Restas sucesivas
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+from tp4ej1 import ingreso_entero
+
+def division_lenta(dividendo, divisor):
+    cociente = 0
+    resto = dividendo
+    while resto >= divisor:
+        resto = resto - divisor
+        cociente = cociente +1
+    return (cociente, resto)
+    if divisor == 0:
+        return 0
+    
+def prueba():
+    
+    print("programa que calcula el cociente y resto de una division mediante restas sucesivas")
+    dividendo = ingreso_entero("Ingrese el dividendo: ")
+    divisor = ingreso_entero("Ingrese el divisor ")
+    (cociente, resto) = division_lenta(dividendo, divisor)
+    print(f"La división entre {dividendo} y {divisor} da {cociente} como cociente y {resto} como resto.")
+    
+    
+if __name__ == "__main__":
+    prueba()
---
+++ tp4ej8.py
@@ -0,0 +1,34 @@
+################
+# Germán Bernhard @GermanBernhard
+# TP4 - Ejercicio 8 - Ordenar 3 valores
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+from tp4ej1 import ingreso_entero
+
+def ordenar_mayor_a_menor(uno, dos, tres):
+    a = min(uno, dos, tres)
+    c = max(uno, dos, tres)
+    b = (uno + dos + tres) - a - c
+
+    return c, b, a
+    
+def ordenar_menor_a_mayor(uno, dos, tres):
+    a = min(uno, dos, tres)
+    c = max(uno, dos, tres)
+    b = (uno + dos + tres) - a - c
+
+    return a, b, c
+
+def prueba():
+    
+    uno = int(input("Escriba el primer numero: "))
+    dos = int(input("Escriba el segundo numero: "))
+    tres = int(input("Escriba el tercer numero: "))
+    menor_mayor = ordenar_menor_a_mayor(uno, dos, tres)
+    mayor_menor = ordenar_mayor_a_menor(uno, dos, tres)
+    print(f"El orden de menor a mayor es {menor_mayor}")
+    print(f"El orden de mayor a menor es {mayor_menor}")
+
+if __name__ == "__main__":
+    prueba()
