f7703de3d65c2beb4c93d16a40dc10147fcc81a2
GxSherman <69094903+GxSherman@users.noreply.github.com>
2021-05-30 11:32:00 -0300

    commit 2

--- tp4ej5.py
+++ tp4ej5.py
@@ -3,21 +3,27 @@
 # TP4 - Ejercicio 5 - Comparación de números
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
+
+
+#Corrección ahora retorna valor numerico en vez de print (1 si es positivo, -1 si es negativo y 0 si es este mismo)
 from tp4ej3 import ingreso_real
 def signo(numero):
     if numero > 0:
-        print("El numero es positivo!")
+        return numero, 1
     elif numero < 0:
-        print("El numero es negativo!")
+        return numero, -1
     else:
-        print("El numero es igual a 0!")       
+        return numero
     
     
 def prueba():
     print("Programa que compara el signo de un numero")
     numero = ingreso_real("Ingrese un numero: ")
-    signo(numero)
-    pass
+    resultado = signo(numero)
+    print("Si el resultado es 1, sera positivo, y si es -1 sera negativo")
+    print(f" El numero ingresado es {resultado} ")
+    
+  
 
 if __name__ == "__main__":
     prueba()
