de71ce299426019a6f9349bab7d28487ecf0e42b
GxSherman <69094903+GxSherman@users.noreply.github.com>
2021-05-30 11:20:53 -0300

    commit

---
+++ tp4ej6.py
@@ -0,0 +1,51 @@
+################
+# Germán Bernhard @GermanBernhard
+# TP4 - Ejercicio 6 - Máximo / Mínimo
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+
+from tp4ej1 import ingreso_entero
+
+
+
+def maximo(lista):
+    
+    maximo = lista[0]
+    
+    for i in range(1, len(lista)):
+        if lista[i] > maximo:
+            maximo = lista[i]
+    return [maximo]
+
+def minimo(lista):
+    
+    minimo = lista[0]
+    
+    for i in range(1, len(lista)):
+        if lista[i] < minimo:
+            minimo = lista[i]
+    return [minimo]
+
+def construccion_lista ():
+    lista=[]
+    for i in range(5):
+        numero = ingreso_entero("Ingrese un valor que sera sumado a la lista")
+        lista.append(numero)
+    return lista
+
+def prueba():
+    print("programa que muestra valor maximo y minimo de una lista")
+    lista = construccion_lista()
+    valor_min = minimo(lista)
+    valor_max = maximo(lista)
+    
+    print("\nLos valores maximos y minimos de la lista son: ")
+    print("Minimo:", valor_min[0])
+    print("Maximo:", valor_max[0])
+    
+    pass
+
+if __name__ == "__main__":
+    prueba()
+
