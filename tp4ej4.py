b3f47bd74b3324d39793bde63d1b8c13dc42feeb
GxSherman <69094903+GxSherman@users.noreply.github.com>
2021-05-30 10:33:29 -0300

    Commit 2

--- tp4ej4.py
+++ tp4ej4.py
@@ -3,24 +3,29 @@
 # TP4 - Ejercicio 4 - Comparación de números
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
+
+#Corregido retornar valores numericos en la funcion y imprimir en prueba
+
 from tp4ej3 import ingreso_real
 
 def compara(numero, otro_numero):
     
     if numero < otro_numero:
-        print("-1, es decir, el segundo numero es mayor que el primero")
+        return -1
     elif numero == otro_numero:
-        print("0, son iguales los numeros!")
+        return 0
     else :
-        print("1, es decir, el primer numero es mayor que el segundo")
+       return 1
         
 def prueba():
     
     print("Programa que compara dos valores de numeros")
     numero = ingreso_real("Ingrese el primer numero:")
     otro_numero = ingreso_real("Ingrese el segundo numero:")
-    compara(numero, otro_numero)
+    valor_final = compara(numero, otro_numero)
+    print('', valor_final)
     pass
+
 
 if __name__ == "__main__":
     prueba()
