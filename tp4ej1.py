38c6934fe851c14d602db1a9bae75bfd1136be08
GxSherman <69094903+GxSherman@users.noreply.github.com>
2021-06-01 12:08:20 -0300

    Commit 2 ej 1 cambio en todo el ejercicio(Aclaro que en este ej me ayudaron con las excepciones ya que estaban bastante complicadas)

--- tp4ej1.py
+++ tp4ej1.py
@@ -1,10 +1,8 @@
 ################
-# Germán Patricio Bernhard Bellini - @GermanBernhard
-# Tp4ej1
+# Germán Bernhard @GermanBernhard
+# TP4 - Ejercicio 1 - Ingreso de números enteros
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
-
-
 
 
 def ingreso_entero(mensaje):
@@ -14,35 +12,60 @@ def ingreso_entero(mensaje):
     """
     try:
         entero = int(input(mensaje + " #"))
-        return entero, True
+        return entero
     except ValueError as e:
-        print("Eso no era un numero")
-        #raise IngresoIncorrecto("Eso no era un numero!") from e
-        return 0, False
-    
-def ingreso_entero_reintento(mensaje, cantidad_reintentos):
-    exito = False
+        print(IngresoIncorrecto("Eso no era un numero, por favor ingrese un numero!!"))
+        return None
+
+def ingreso_entero_reintento(mensaje, cantidad_reintentos=5):
+  
     cantidad_intentos = 0
-    while exito == False and cantidad_intentos < cantidad_reintentos:
-        entero, exito = ingreso_entero(mensaje)
-        cantidad_reintentos = cantidad_reintentos -1
+    entero = None
+    while entero == None and cantidad_intentos < cantidad_reintentos:
+        entero = ingreso_entero(mensaje)
+        cantidad_reintentos = cantidad_reintentos - 1
+        if entero == None:
+            print(F"Le quedan {cantidad_reintentos} intentos para ingresar numeros.\n")
     if cantidad_intentos == cantidad_reintentos:
-        raise NoMasReintentos("Se quedó sin intentos")
+        raise No_Mas_Reintentos("Se quedó sin intentos, reinicie el programa")
     return entero
+
+
 def ingreso_entero_restringido(mensaje, valor_minimo=0, valor_maximo=10):
-    pass
-class NoMasReintentos(Exception):
+    
+    entero = ingreso_entero(mensaje)
+    try:
+        if entero >= valor_minimo and entero <= valor_maximo:
+            return entero
+        else:
+            raise Fuera_De_Rango("El numero ingresado está fuera del rango determinado.")
+            pass
+    except TypeError as e:
+        raise TypeError("Los valores ingresados no eran numeros, ingrese un numero por favor") from e
+    
+class No_Mas_Reintentos(Exception):
     """Esta es la Excepcion para la falta de reintentos"""
     pass
 
-class IngresoIncorrecto(Exception):
+class Fuera_De_Rango(Exception):
+    """Esta es la Excepcion para cuando se ingresa un número fuera del rango establecido por el usuario"""
+    pass
+
+class Ingreso_Incorrecto(Exception):
     """Esta es la Excepcion para el ingreso incorrecto"""
     pass
+
+
 def prueba():
-    entero = ingreso_entero_reintento("Ingrese un numero", 5)
-    #entero = ingreso_entero("ingrese un numero")
-    print(F"el numero ingresado es {entero}")  
-    
+    print("Programa que determina si un numero es entero o no")
+    mensaje = "Ingrese un numero"
+    valor_minimo = ingreso_entero("Ingrese el valor minimo")
+    valor_maximo = ingreso_entero("Ingrese el valor maximo")
+    numero_entero = ingreso_entero_restringido(mensaje,valor_minimo,valor_maximo)
+
+    print(F"el numero que ingreso es {numero_entero}")
+
+
 if __name__ == "__main__":
-     prueba()
- 
+    prueba()
+    
